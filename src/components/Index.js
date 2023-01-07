507df6dc9074d881a2a47a82436cc2fbd20d2d44
ellzmarie <elliematutisdigal@gmail.com>
2023-01-07 14:31:13 -0800

    updates Index.js file

--- src/components/Index.js
+++ src/components/Index.js
@@ -6,6 +6,24 @@ function Index(props){
         name: '',
         url: ''
     })
+
+    const loaded = () => {
+        return props.site.map((site) => (
+            <div key={site._id} className='site'>
+
+                <Link to={`/site/${site._id}`}>
+                    <h1>{site.name}</h1>
+                </Link>
+
+            </div>
+        ))
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        props.createSite(newForm)
+        setNewSite
+    }
 }
 
 export default Index
