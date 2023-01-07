228dedbe6a16b8d6b997ffb4f6d17bee97c9331b
ellzmarie <elliematutisdigal@gmail.com>
2023-01-07 11:29:07 -0800

    updates folder name

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,9 @@
+import {useEffect, useState} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from '../components/Footer'
+import Show from '../components/Header'
+
+
 function Home(props) {
     return <h1>HOME</h1>;
   }
